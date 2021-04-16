1be9da20d676b085c5d9b6e79cf391fd04582df9
charkster <31560568+charkster@users.noreply.github.com>
2021-04-15 18:27:04 -0700

    Create test_test_equity_107_usb.py

---
+++ test_test_equity_107_usb.py
@@ -0,0 +1,11 @@
+from test_equity_107_usb import test_equity_107_usb
+
+tc = test_equity_107_usb(serial_port='/dev/ttyUSB0') # serial_port='COM6' for Windows
+print(tc.read_temp())
+tc.write_temp_value(24.7)
+tc.start_chamber()
+time.sleep(1)
+tc.stop_chamber()
+time.sleep(1)
+tc.set_temp(26)
+print("Temperature has now been reached")
