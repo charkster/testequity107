84d3ed02ba08a86662476e8b0a63b664c55b6f14
charkster <31560568+charkster@users.noreply.github.com>
2021-04-19 13:03:11 -0700

    Update test_equity_107_usb.py

--- test_equity_107_usb.py
+++ test_equity_107_usb.py
@@ -42,26 +42,36 @@ class test_equity_107_usb():
 	def stop_chamber(self):
 		self.inst.write_register(self.register_set_enable, 0, 0)
 	
-	def set_temp(self, target_temp, guardband=1, soak_time=1, stable_cnt=10):
+		# guardband is degrees C +/- from the target temp, soak_time is in minutes, 
+	# stable_cnt is the number of consective reads needed to be stable
+	# sample_interval is the number is seconds between each reading of temperature
+	def set_temp(self, target_temp, guardband=1, soak_time=1, stable_cnt=10, sample_interval=1):
 		self.write_temp_value(target_temp)
 		self.start_chamber()
 		stable = False
 		cnt    = 0
-		print("Target temperature is " + str(target_temp) + ", Guardband is " + str(guardband))
+		start_time = time.time()
+		print("Target temperature is " + str(target_temp) + "C, Guardband is " + str(guardband) + "C, Sample interval is " + str(sample_interval) + " seconds, Stable count is " + str(stable_cnt))
 		while not stable:
-			time.sleep(1)
+			time.sleep(sample_interval)
 			current_temp = self.read_temp()
-			print(current_temp)
 			if (target_temp - guardband) <= current_temp <= (target_temp + guardband):
 				cnt += 1
+				print(str(current_temp) + " (cnt={})".format(cnt))
 			else:
 				cnt = 0
+				print(current_temp)
 			if (cnt == stable_cnt):
 				stable = True
+		print("Soak for {:0.2f} minutes before proceeding...".format(soak_time))
 		while soak_time >= 1.0:
-			print("Wait {:0.2f} minutes before measurement...".format(soak_time))
 			time.sleep(60)
 			soak_time -= 1.0
+			print("Soak for {:0.2f} minutes before proceeding...".format(soak_time))
 		if soak_time > 0.0:
 			time.sleep(60.0 * soak_time)
-		print("Chamber is at temperature")
+		current_temp = self.read_temp()
+		stop_time = time.time()
+		elapsed_minutes = (stop_time - start_time) / 60.0
+		print("Chamber is at temperature, current temp is {:0.1f}, target temp is {:0.1f}, guardband is {:0.1f}, elapsed time is {:0.2f} minutes".format(current_temp,target_temp,guardband,elapsed_minutes))
+		
